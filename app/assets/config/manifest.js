ac2ef17dae615842fa7f39428adc338ddd0b9038
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-08-16 14:34:40 +0200

    Update importmap-rails usage (#13)
    * bump importmap-rails to 0.2.2
    
    * Update importmap-rails usage
    
    importmap is configured in dedicated initializer.
    paths is replaced with pin & pin_all
    
    * bump stimulus-rails to 0.3.3 and turbo-rails to 0.7.2
    
    These are now using the new api for importmap-rails
    
    * Remove not needed pin_all_from
    
    The defaults load all from app/assets/javascripts

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../javascripts
+//= link_tree ../javascripts .js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,16 +10,3 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-# Configure importmap paths in addition to having all files in app/assets/javascripts mapped.
-Rails.application.config.importmap.paths.tap do |paths|
-  # Match libraries with their NPM package names for possibility of easy later porting.
-  # Ensure that libraries listed in the path have been linked in the asset pipeline manifest or precompiled.
-
-  # paths.asset "@rails/actioncable", path: "actioncable.esm.js"
-  # paths.asset "@rails/actiontext", path: "actiontext.js"
-  # paths.asset "@rails/activestorage", path: "activestorage.esm.js"
-
-  # Make all files in directory available as my_channel => channels/my_channel-$digest.js
-  paths.assets_in "lib/assets/javascripts/libraries", append_base_path: true
-end
