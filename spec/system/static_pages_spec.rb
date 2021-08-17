b690cf23b4ab866ba2909858e89d931d8a33d401
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-08-17 10:35:15 +0200

    Test that our stimulus controller works

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Static pages" do
-  it "visiting index" do
+  it "visiting index", js: true do
     visit root_path
     expect(page).to have_content("Welcome")
+    expect(page).to have_content("Hello World From StimulusController!")
   end
 end
