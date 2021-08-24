cedbc5fa92612dd5e455c507fce7fb27166d630d
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-08-24 13:26:43 +0200

    Add better-rails-system-specs and reorganize specs (#18)
    * Add better-rails-system-specs and reorganize specs
    
    * We are running js by default for system specs
    
    * StandardRb

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,7 +1,7 @@
-require "rails_helper"
+require "system_helper"
 
 RSpec.describe "Static pages" do
-  it "visiting index", js: true do
+  it "visiting index" do
     visit root_path
     expect(page).to have_content("Welcome")
     expect(page).to have_content("Hello World From StimulusController!")
