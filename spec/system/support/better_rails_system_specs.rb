0696fafccb01510b311864097e3a38c1d4ac491b
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-08-24 13:26:43 +0200

    Add better-rails-system-specs and reorganize specs (#18)
    * Add better-rails-system-specs and reorganize specs
    
    * We are running js by default for system specs
    
    * StandardRb

---
+++ spec/system/support/better_rails_system_specs.rb
@@ -0,0 +1,37 @@
+# Rails is not flexible enough; at least for now(
+module BetterRailsSystemTests
+  # Use our `Capybara.save_path` to store screenshots with other capybara artifacts
+  # (Rails screenshots path is not configurable https://github.com/rails/rails/blob/49baf092439fc74fc3377b12e3334c3dd9d0752f/actionpack/lib/action_dispatch/system_testing/test_helpers/screenshot_helper.rb#L79)
+  def absolute_image_path
+    Rails.root.join("#{Capybara.save_path}/screenshots/#{image_name}.png")
+  end
+
+  # Use relative path in screenshot message to make it clickable in VS Code when running in Docker
+  def image_path
+    absolute_image_path.relative_path_from(Rails.root).to_s
+  end
+
+  # Convert dom_id to selector
+  def dom_id(*args)
+    "##{super}"
+  end
+end
+
+RSpec.configure do |config|
+  # Add #dom_id support
+  config.include ActionView::RecordIdentifier, type: :system
+  config.include BetterRailsSystemTests, type: :system
+
+  # Make urls in mailers contain the correct server host
+  config.around(:each, type: :system) do |ex|
+    was_host, Rails.application.default_url_options[:host] = Rails.application.default_url_options[:host], Capybara.server_host
+    ex.run
+    Rails.application.default_url_options[:host] = was_host
+  end
+
+  # Make sure this hook runs before others
+  config.prepend_before(:each, type: :system) do
+    # Use JS driver always
+    driven_by Capybara.javascript_driver
+  end
+end
---
+++ spec/system_helper.rb
@@ -0,0 +1,4 @@
+require "rails_helper"
+
+# system/support/ files contain system tests configurations and helpers
+Dir[File.join(__dir__, "system/support/**/*.rb")].sort.each { |file| require file }
