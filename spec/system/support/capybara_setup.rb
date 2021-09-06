e55bb3037d85bbb5097ed0e876c80d7b4e95d031
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-09-06 16:54:58 +0200

    Use standalone-firefox and refactor config

--- spec/system/support/capybara_setup.rb
+++ spec/system/support/capybara_setup.rb
@@ -2,17 +2,17 @@ require "capybara/rspec"
 
 Capybara.server = :puma, {Silent: true}
 
-Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
+Capybara.register_driver :headless do |app|
+  options = ::Selenium::WebDriver::Firefox::Options.new
   options.add_argument("--no-sandbox")
   options.add_argument("--headless")
   options.add_argument("--disable-gpu")
   options.add_argument("--window-size=2800,2800")
 
-  if ENV["CHROME_URL"]
+  if ENV["REMOTE_SELENIUM_URL"]
     Capybara::Selenium::Driver.new(app,
       browser: :remote,
-      url: ENV["CHROME_URL"],
+      url: ENV["REMOTE_SELENIUM_URL"],
       capabilities: options)
   else
     require "webdrivers"
@@ -24,7 +24,7 @@ end
 
 RSpec.configure do |config|
   config.before(:each, type: :system) {
-    driven_by :chrome_headless
+    driven_by :headless
 
     capybara_host = IPSocket.getaddress(Socket.gethostname)
 
