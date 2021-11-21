ccc0ce538718a841127546595c1126d92268b30a
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-11-21 10:41:59 +0100

    Setup LetterOpener by default in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
